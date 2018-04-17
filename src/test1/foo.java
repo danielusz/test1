3ddf27a489bd76d3668a44d48709899270b4f417
danielusz <danielusz@gmail.com>
2018-04-17 14:55:00 +0200

    Update foo.java

--- src/test1/foo.java
+++ src/test1/foo.java
@@ -5,8 +5,10 @@ public class foo {
 	public static void main(String[] args) {
 		StringBuilder sBilder = new StringBuilder();
 		sBilder.append("Hello ");
-		sBilder.append("world!");
-
+		sBilder.append("Java ");
+		sBilder.append("World!");
+		
+		
 		System.out.println(sBilder.toString());
 	}
 
